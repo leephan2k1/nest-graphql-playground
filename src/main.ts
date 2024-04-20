6d8a2a607d1b8f0017cb67eba08e30a8e12c15bc
leephan2k1 <pttl2k1@gmail.com>
2024-04-21 00:01:45 +0700

    add simple security layer

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,47 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  const env = configService.get('NODE_ENV');
+  const clientUrl = configService.get('CLIENT_URL');
+
+  if (env !== 'local') {
+    console.info('Security setup: ', {
+      env,
+      clientUrl,
+    });
+
+    app.enableCors({
+      origin: clientUrl || `http://localhost:4200`,
+    });
+
+    app.use(
+      helmet({
+        crossOriginEmbedderPolicy: false,
+        contentSecurityPolicy: {
+          directives: {
+            imgSrc: [
+              `'self'`,
+              'data:',
+              'apollo-server-landing-page.cdn.apollographql.com',
+            ],
+            scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+            manifestSrc: [
+              `'self'`,
+              'apollo-server-landing-page.cdn.apollographql.com',
+            ],
+            frameSrc: [`'self'`, 'sandbox.embed.apollographql.com'],
+          },
+        },
+      }),
+    );
+  }
+
   await app.listen(3000);
 }
 bootstrap();
